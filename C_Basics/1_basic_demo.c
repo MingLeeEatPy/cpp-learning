919857f12091305e69ea39af7fd4e0b7c8f2be48
Ming Lee <2442066641@qq.com>
2026-08-16 19:58:39 +0800

    binary note

--- C_Basics/1_basic_demo.c
+++ C_Basics/1_basic_demo.c
@@ -3,6 +3,6 @@
 int main(){
     printf("Hello, World!\n");
     printf("She said \"OK\"\n你是最棒的\n");
-    printf("His favorite song is \"Lemon Tree\"\n C:\\Users\\Example");
+    printf("His favorite song is \"Lemon Tree\"\n C:\\Users\\Example\a");
     return 0;
 }
---
+++ C_Basics/2a_printf_demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("==================================================\n");
+    printf("This is a \"double quote\" and this is a backslash: \\.\n");
+    printf("==================================================\n");
+    printf("Name\tAge\tCity\n");
+    printf("John\t25\tNew York\n");
+    printf("Jane\t30\tLos Angeles\n");
+
+    return 0;
+}
