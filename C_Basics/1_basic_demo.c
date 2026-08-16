283431f765b04dd3ed14a4cecda8f8783510b6ce
Ming Lee <2442066641@qq.com>
2026-08-16 19:03:17 +0800

    Add C basic syntax practice

---
+++ C_Basics/1_basic_demo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    printf("She said \"OK\"\n你是最棒的\n");
+    printf("His favorite song is \"Lemon Tree\"\n C:\\Users\\Example");
+    return 0;
+}
---
+++ C_Basics/2_printf_plus.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("商品编号\t商品名称\t商品价格\n");
+    printf("01\t\t\tegg\t\t\t5.00\n");
+    printf("02\t\t\tmilk\t\t10.00\n");
+    printf("03\t\t\tbread\t\t8.00\n");
+    return 0;
+}
